472cca9b863d7e608985d85c734f323dbf47a25c
ngrwisnu <adiprayogongrwisnu@gmail.com>
2023-10-27 16:43:55 +0700

    docker health check

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,15 @@ FROM node:18.18.0-alpine
 
 ENV PORT=3000
 
+RUN apk --no-cache add curl
 WORKDIR /app
 
 COPY . /app
 RUN npm install
 
 EXPOSE ${PORT}
+
+HEALTHCHECK --interval=5s --start-period=5s CMD curl -f http://localhost:3000/health-check
 
 ENTRYPOINT [ "npm", "run" ]
 
--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
+let counter = 0;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/health-check", (req, res) => {
+  counter += 1;
+
+  if (counter > 3) {
+    res.status(400).send("Express is Sick!");
+  } else {
+    res.status(200).send(`Let's count again (${counter})`);
+  }
 });
 
 app.listen(port, () => {
